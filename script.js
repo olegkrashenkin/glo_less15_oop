4d0f53e45b69ed6369b14ecc26d01efcab8abd19
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2022-12-26 01:36:02 +0300

    сделал обычное задание

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 'use strict'
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+    this.selector = selector
+    this.height = height
+    this.width = width
+    this.bg = bg
+    this.fontSize = fontSize
+}
+
+DomElement.prototype.createEl = function () {
+    switch (this.selector[0]) {
+        case '#':
+            const p = document.createElement('p')
+            p.id = this.selector.slice(1)
+            p.style.cssText =
+                `height: ${this.height}; width: ${this.width};
+                background: ${this.bg}; font-size: ${this.fontSize}`
+            p.textContent =
+                `Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Laboriosam, at beatae! Accusantium eligendi at id!`
+            document.body.append(p)
+            break;
+
+        case '.':
+            const div = document.createElement('div')
+            div.classList.add(this.selector.slice(1))
+            div.style.cssText =
+                `height: ${this.height}; width: ${this.width};
+                background: ${this.bg}; font-size: ${this.fontSize}`
+            div.textContent =
+                `Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Laboriosam, at beatae! Accusantium eligendi at id!`
+            document.body.append(div)
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+const test = new DomElement('#new_class', '100px', '200px', 'green', '18')
+
+test.createEl()
